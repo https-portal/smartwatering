e121755db8ccc12d33b47f8165cf623c1cdb1252
https-portal <62538961+https-portal@users.noreply.github.com>
2026-02-10 13:05:26 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,26 @@
-// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyDptVEqHvZqOF1deQrOpnhQI_w6Vb5n4zQ",
   authDomain: "watersystem-e444e.firebaseapp.com",
+  databaseURL: "https://watersystem-e444e-default-rtdb.firebaseio.com",
   projectId: "watersystem-e444e",
+  storageBucket: "watersystem-e444e.appspot.com",
   messagingSenderId: "528910169669",
-  appId: "1:528910169669:web:05c99d2ca2e6d8c069952c"
+  appId: "1:528910169669:web:05c99d2ca2e6d8c069952c",
 });
 
 const messaging = firebase.messaging();
 
-// Background notifications
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const { title, body } = payload.notification;
-  self.registration.showNotification(title, { body });
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png'  // optional
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
